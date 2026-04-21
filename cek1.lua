dcc5611ac1bf384013f24dc27a7cffd18f6d1b3c
Bimaaa <kokmox021005@gmail.com>
2026-04-21 20:45:47 +0700

    Delete cek1.lua

--- cek1.lua
+++
@@ -1,6 +0,0 @@
-print("HTTP TEST START")
-
-local data = game:HttpGet("https://raw.githubusercontent.com/Groot2k/GrowAGardenScripts/main/cek1.lua", true)
-print("HTTP OK, PANJANG DATA:", #data)
-
-loadstring(data)()
